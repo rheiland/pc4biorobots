a424b80614bc1a0e74821660ce38d6e7638a26eb
Randy Heiland <heiland@indiana.edu>
2019-11-14 18:49:27 -0500

    updates

--- data/user_params.py
+++ data/user_params.py
@@ -33,126 +33,94 @@ class UserTab(object):
           step=1,
           style=style, layout=widget_layout)
 
-        param_name2 = Button(description='cargo_signal_D', disabled=True, layout=name_button_layout)
+        param_name2 = Button(description='elastic_coefficient', disabled=True, layout=name_button_layout)
         param_name2.style.button_color = 'tan'
-
-        self.cargo_signal_D = FloatText(
-          value=1e3,
-          step=100,
-          style=style, layout=widget_layout)
-
-        param_name3 = Button(description='cargo_signal_decay', disabled=True, layout=name_button_layout)
-        param_name3.style.button_color = 'lightgreen'
-
-        self.cargo_signal_decay = FloatText(
-          value=.4,
-          step=0.1,
-          style=style, layout=widget_layout)
-
-        param_name4 = Button(description='director_signal_D', disabled=True, layout=name_button_layout)
-        param_name4.style.button_color = 'tan'
-
-        self.director_signal_D = FloatText(
-          value=1e3,
-          step=100,
-          style=style, layout=widget_layout)
-
-        param_name5 = Button(description='director_signal_decay', disabled=True, layout=name_button_layout)
-        param_name5.style.button_color = 'lightgreen'
-
-        self.director_signal_decay = FloatText(
-          value=.1,
-          step=0.01,
-          style=style, layout=widget_layout)
-
-        param_name6 = Button(description='elastic_coefficient', disabled=True, layout=name_button_layout)
-        param_name6.style.button_color = 'tan'
 
         self.elastic_coefficient = FloatText(
           value=0.05,
           step=0.01,
           style=style, layout=widget_layout)
 
-        param_name7 = Button(description='worker_motility_persistence_time', disabled=True, layout=name_button_layout)
-        param_name7.style.button_color = 'lightgreen'
+        param_name3 = Button(description='worker_motility_persistence_time', disabled=True, layout=name_button_layout)
+        param_name3.style.button_color = 'lightgreen'
 
         self.worker_motility_persistence_time = FloatText(
           value=5.0,
           step=0.1,
           style=style, layout=widget_layout)
 
-        param_name8 = Button(description='worker_migration_speed', disabled=True, layout=name_button_layout)
-        param_name8.style.button_color = 'tan'
+        param_name4 = Button(description='worker_migration_speed', disabled=True, layout=name_button_layout)
+        param_name4.style.button_color = 'tan'
 
         self.worker_migration_speed = FloatText(
           value=5.0,
           step=0.1,
           style=style, layout=widget_layout)
 
-        param_name9 = Button(description='attached_worker_migration_bias', disabled=True, layout=name_button_layout)
-        param_name9.style.button_color = 'lightgreen'
+        param_name5 = Button(description='attached_worker_migration_bias', disabled=True, layout=name_button_layout)
+        param_name5.style.button_color = 'lightgreen'
 
         self.attached_worker_migration_bias = FloatText(
           value=1.0,
           step=0.1,
           style=style, layout=widget_layout)
 
-        param_name10 = Button(description='unattached_worker_migration_bias', disabled=True, layout=name_button_layout)
-        param_name10.style.button_color = 'tan'
+        param_name6 = Button(description='unattached_worker_migration_bias', disabled=True, layout=name_button_layout)
+        param_name6.style.button_color = 'tan'
 
         self.unattached_worker_migration_bias = FloatText(
           value=0.5,
           step=0.1,
           style=style, layout=widget_layout)
 
-        param_name11 = Button(description='number_of_directors', disabled=True, layout=name_button_layout)
-        param_name11.style.button_color = 'lightgreen'
+        param_name7 = Button(description='number_of_directors', disabled=True, layout=name_button_layout)
+        param_name7.style.button_color = 'lightgreen'
 
         self.number_of_directors = IntText(
           value=15,
           step=1,
           style=style, layout=widget_layout)
 
-        param_name12 = Button(description='number_of_cargo_clusters', disabled=True, layout=name_button_layout)
-        param_name12.style.button_color = 'tan'
+        param_name8 = Button(description='number_of_cargo_clusters', disabled=True, layout=name_button_layout)
+        param_name8.style.button_color = 'tan'
 
         self.number_of_cargo_clusters = IntText(
           value=100,
           step=10,
           style=style, layout=widget_layout)
 
-        param_name13 = Button(description='number_of_workers', disabled=True, layout=name_button_layout)
-        param_name13.style.button_color = 'lightgreen'
+        param_name9 = Button(description='number_of_workers', disabled=True, layout=name_button_layout)
+        param_name9.style.button_color = 'lightgreen'
 
         self.number_of_workers = IntText(
           value=50,
           step=1,
           style=style, layout=widget_layout)
 
-        param_name14 = Button(description='drop_threshold', disabled=True, layout=name_button_layout)
-        param_name14.style.button_color = 'tan'
+        param_name10 = Button(description='drop_threshold', disabled=True, layout=name_button_layout)
+        param_name10.style.button_color = 'tan'
 
         self.drop_threshold = FloatText(
           value=0.4,
           step=0.1,
           style=style, layout=widget_layout)
 
-        param_name15 = Button(description='worker_color', disabled=True, layout=name_button_layout)
-        param_name15.style.button_color = 'lightgreen'
+        param_name11 = Button(description='worker_color', disabled=True, layout=name_button_layout)
+        param_name11.style.button_color = 'lightgreen'
 
         self.worker_color = Text(
           value='red',
           style=style, layout=widget_layout)
 
-        param_name16 = Button(description='cargo_color', disabled=True, layout=name_button_layout)
-        param_name16.style.button_color = 'tan'
+        param_name12 = Button(description='cargo_color', disabled=True, layout=name_button_layout)
+        param_name12.style.button_color = 'tan'
 
         self.cargo_color = Text(
           value='blue',
           style=style, layout=widget_layout)
 
-        param_name17 = Button(description='director_color', disabled=True, layout=name_button_layout)
-        param_name17.style.button_color = 'lightgreen'
+        param_name13 = Button(description='director_color', disabled=True, layout=name_button_layout)
+        param_name13.style.button_color = 'lightgreen'
 
         self.director_color = Text(
           value='limegreen',
@@ -160,19 +128,19 @@ class UserTab(object):
 
         units_button1 = Button(description='', disabled=True, layout=units_button_layout) 
         units_button1.style.button_color = 'lightgreen'
-        units_button2 = Button(description='micron/min^2', disabled=True, layout=units_button_layout) 
+        units_button2 = Button(description='1/min', disabled=True, layout=units_button_layout) 
         units_button2.style.button_color = 'tan'
-        units_button3 = Button(description='1/min', disabled=True, layout=units_button_layout) 
+        units_button3 = Button(description='min', disabled=True, layout=units_button_layout) 
         units_button3.style.button_color = 'lightgreen'
-        units_button4 = Button(description='micron/min^2', disabled=True, layout=units_button_layout) 
+        units_button4 = Button(description='micron/min', disabled=True, layout=units_button_layout) 
         units_button4.style.button_color = 'tan'
-        units_button5 = Button(description='1/min', disabled=True, layout=units_button_layout) 
+        units_button5 = Button(description='', disabled=True, layout=units_button_layout) 
         units_button5.style.button_color = 'lightgreen'
-        units_button6 = Button(description='1/min', disabled=True, layout=units_button_layout) 
+        units_button6 = Button(description='', disabled=True, layout=units_button_layout) 
         units_button6.style.button_color = 'tan'
-        units_button7 = Button(description='min', disabled=True, layout=units_button_layout) 
+        units_button7 = Button(description='', disabled=True, layout=units_button_layout) 
         units_button7.style.button_color = 'lightgreen'
-        units_button8 = Button(description='micron/min', disabled=True, layout=units_button_layout) 
+        units_button8 = Button(description='', disabled=True, layout=units_button_layout) 
         units_button8.style.button_color = 'tan'
         units_button9 = Button(description='', disabled=True, layout=units_button_layout) 
         units_button9.style.button_color = 'lightgreen'
@@ -184,67 +152,47 @@ class UserTab(object):
         units_button12.style.button_color = 'tan'
         units_button13 = Button(description='', disabled=True, layout=units_button_layout) 
         units_button13.style.button_color = 'lightgreen'
-        units_button14 = Button(description='', disabled=True, layout=units_button_layout) 
-        units_button14.style.button_color = 'tan'
-        units_button15 = Button(description='', disabled=True, layout=units_button_layout) 
-        units_button15.style.button_color = 'lightgreen'
-        units_button16 = Button(description='', disabled=True, layout=units_button_layout) 
-        units_button16.style.button_color = 'tan'
-        units_button17 = Button(description='', disabled=True, layout=units_button_layout) 
-        units_button17.style.button_color = 'lightgreen'
 
         desc_button1 = Button(description='', disabled=True, layout=desc_button_layout) 
         desc_button1.style.button_color = 'lightgreen'
-        desc_button2 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button2 = Button(description='strength of worker-cargo adhesion', disabled=True, layout=desc_button_layout) 
         desc_button2.style.button_color = 'tan'
-        desc_button3 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button3 = Button(description='time between direction changes for workers', disabled=True, layout=desc_button_layout) 
         desc_button3.style.button_color = 'lightgreen'
         desc_button4 = Button(description='', disabled=True, layout=desc_button_layout) 
         desc_button4.style.button_color = 'tan'
-        desc_button5 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button5 = Button(description='how strictly attached workers follow chemotactic signals', disabled=True, layout=desc_button_layout) 
         desc_button5.style.button_color = 'lightgreen'
-        desc_button6 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button6 = Button(description='how strictly unattached workers follow chemotactic signals', disabled=True, layout=desc_button_layout) 
         desc_button6.style.button_color = 'tan'
-        desc_button7 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button7 = Button(description='number of director cells', disabled=True, layout=desc_button_layout) 
         desc_button7.style.button_color = 'lightgreen'
-        desc_button8 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button8 = Button(description='number of 7-cell cargo clusters', disabled=True, layout=desc_button_layout) 
         desc_button8.style.button_color = 'tan'
-        desc_button9 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button9 = Button(description='number of worker cells', disabled=True, layout=desc_button_layout) 
         desc_button9.style.button_color = 'lightgreen'
-        desc_button10 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button10 = Button(description='drop cargo when director signal exceeds this level', disabled=True, layout=desc_button_layout) 
         desc_button10.style.button_color = 'tan'
-        desc_button11 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button11 = Button(description='color for worker cells (see W3C colors)', disabled=True, layout=desc_button_layout) 
         desc_button11.style.button_color = 'lightgreen'
-        desc_button12 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button12 = Button(description='color for cargo cells', disabled=True, layout=desc_button_layout) 
         desc_button12.style.button_color = 'tan'
-        desc_button13 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button13 = Button(description='color for director cells', disabled=True, layout=desc_button_layout) 
         desc_button13.style.button_color = 'lightgreen'
-        desc_button14 = Button(description='', disabled=True, layout=desc_button_layout) 
-        desc_button14.style.button_color = 'tan'
-        desc_button15 = Button(description='', disabled=True, layout=desc_button_layout) 
-        desc_button15.style.button_color = 'lightgreen'
-        desc_button16 = Button(description='', disabled=True, layout=desc_button_layout) 
-        desc_button16.style.button_color = 'tan'
-        desc_button17 = Button(description='', disabled=True, layout=desc_button_layout) 
-        desc_button17.style.button_color = 'lightgreen'
 
         row1 = [param_name1, self.random_seed, units_button1, desc_button1] 
-        row2 = [param_name2, self.cargo_signal_D, units_button2, desc_button2] 
-        row3 = [param_name3, self.cargo_signal_decay, units_button3, desc_button3] 
-        row4 = [param_name4, self.director_signal_D, units_button4, desc_button4] 
-        row5 = [param_name5, self.director_signal_decay, units_button5, desc_button5] 
-        row6 = [param_name6, self.elastic_coefficient, units_button6, desc_button6] 
-        row7 = [param_name7, self.worker_motility_persistence_time, units_button7, desc_button7] 
-        row8 = [param_name8, self.worker_migration_speed, units_button8, desc_button8] 
-        row9 = [param_name9, self.attached_worker_migration_bias, units_button9, desc_button9] 
-        row10 = [param_name10, self.unattached_worker_migration_bias, units_button10, desc_button10] 
-        row11 = [param_name11, self.number_of_directors, units_button11, desc_button11] 
-        row12 = [param_name12, self.number_of_cargo_clusters, units_button12, desc_button12] 
-        row13 = [param_name13, self.number_of_workers, units_button13, desc_button13] 
-        row14 = [param_name14, self.drop_threshold, units_button14, desc_button14] 
-        row15 = [param_name15, self.worker_color, units_button15, desc_button15] 
-        row16 = [param_name16, self.cargo_color, units_button16, desc_button16] 
-        row17 = [param_name17, self.director_color, units_button17, desc_button17] 
+        row2 = [param_name2, self.elastic_coefficient, units_button2, desc_button2] 
+        row3 = [param_name3, self.worker_motility_persistence_time, units_button3, desc_button3] 
+        row4 = [param_name4, self.worker_migration_speed, units_button4, desc_button4] 
+        row5 = [param_name5, self.attached_worker_migration_bias, units_button5, desc_button5] 
+        row6 = [param_name6, self.unattached_worker_migration_bias, units_button6, desc_button6] 
+        row7 = [param_name7, self.number_of_directors, units_button7, desc_button7] 
+        row8 = [param_name8, self.number_of_cargo_clusters, units_button8, desc_button8] 
+        row9 = [param_name9, self.number_of_workers, units_button9, desc_button9] 
+        row10 = [param_name10, self.drop_threshold, units_button10, desc_button10] 
+        row11 = [param_name11, self.worker_color, units_button11, desc_button11] 
+        row12 = [param_name12, self.cargo_color, units_button12, desc_button12] 
+        row13 = [param_name13, self.director_color, units_button13, desc_button13] 
 
         box_layout = Layout(display='flex', flex_flow='row', align_items='stretch', width='100%')
         box1 = Box(children=row1, layout=box_layout)
@@ -260,10 +208,6 @@ class UserTab(object):
         box11 = Box(children=row11, layout=box_layout)
         box12 = Box(children=row12, layout=box_layout)
         box13 = Box(children=row13, layout=box_layout)
-        box14 = Box(children=row14, layout=box_layout)
-        box15 = Box(children=row15, layout=box_layout)
-        box16 = Box(children=row16, layout=box_layout)
-        box17 = Box(children=row17, layout=box_layout)
 
         self.tab = VBox([
           box1,
@@ -279,10 +223,6 @@ class UserTab(object):
           box11,
           box12,
           box13,
-          box14,
-          box15,
-          box16,
-          box17,
         ])
 
     # Populate the GUI widgets with values from the XML
@@ -295,10 +235,6 @@ class UserTab(object):
 
         uep = xml_root.find('.//user_parameters')  # find unique entry point
         self.random_seed.value = int(uep.find('.//random_seed').text)
-        self.cargo_signal_D.value = float(uep.find('.//cargo_signal_D').text)
-        self.cargo_signal_decay.value = float(uep.find('.//cargo_signal_decay').text)
-        self.director_signal_D.value = float(uep.find('.//director_signal_D').text)
-        self.director_signal_decay.value = float(uep.find('.//director_signal_decay').text)
         self.elastic_coefficient.value = float(uep.find('.//elastic_coefficient').text)
         self.worker_motility_persistence_time.value = float(uep.find('.//worker_motility_persistence_time').text)
         self.worker_migration_speed.value = float(uep.find('.//worker_migration_speed').text)
@@ -323,10 +259,6 @@ class UserTab(object):
 
         uep = xml_root.find('.//user_parameters')  # find unique entry point
         uep.find('.//random_seed').text = str(self.random_seed.value)
-        uep.find('.//cargo_signal_D').text = str(self.cargo_signal_D.value)
-        uep.find('.//cargo_signal_decay').text = str(self.cargo_signal_decay.value)
-        uep.find('.//director_signal_D').text = str(self.director_signal_D.value)
-        uep.find('.//director_signal_decay').text = str(self.director_signal_decay.value)
         uep.find('.//elastic_coefficient').text = str(self.elastic_coefficient.value)
         uep.find('.//worker_motility_persistence_time').text = str(self.worker_motility_persistence_time.value)
         uep.find('.//worker_migration_speed').text = str(self.worker_migration_speed.value)
